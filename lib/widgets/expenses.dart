3ba453d6ebd677032cc7a46d7673656ba99932c6
vipin-v <vipindas233@gmail.com>
2024-03-30 13:34:12 +0530

    ScaffoldMessenger snackbar fixed

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -45,6 +45,8 @@ class _ExpensesState extends State<Expenses> {
     setState(() {
       _registeredExpenses.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         duration: const Duration(seconds: 5),
